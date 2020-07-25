24a67f744945f6472e7b3bb09f59217b91fca658
Kurt LaVacque <lavacquek@icloud.com>
2020-07-25 14:30:53 -0500

    Updated teh Sparkle Rainbow Mode

--- Sparkle_Rainbow.ino
+++ Sparkle_Rainbow.ino
@@ -1,20 +1,35 @@
 // Pattern 5
-#define STROBE_5 0.5
+#define STROBE_5 20
 #define GAP_5 0
+#define RATE_5 10
+int start_hue_5;
+int delta_hue_5 = 5;
 
 void sparkle_rainbow()
 {
+  start_hue_5 = -1 * millis() / RATE_5;
   Serial.println("pattern_5");
   for (int i = 0; i < NUM_LEDS; i++)
   {
     for (int i = 0; i < 10; i++)
     {
-      leds[random(NUM_LEDS)] = CHSV(random(0, 255), 255, random(0, 255)); // Change To Random LED and change color to White
-      delay(STROBE_5);
+      int random_leds[10];
+      for (int i = 0; i < 10; i++)
+      {
+        random_leds[i] = random(NUM_LEDS);
+      }
+      for (int i = 0; i < 10; i++)
+      {
+        leds[random_leds[i]] = CHSV(start_hue_5, 255, random(0, 255)); // Change To Random LED and change color to White
+      }
+      FastLED.show();
+      FastLED.delay(STROBE_5);
+      for (int i = 0; i < 10; i++)
+      {
+        leds[random_leds[i]] = CHSV(0, 0, 0); // Change To Random LED and change color to White
+      }
       FastLED.show();
     }
-    fill_solid(leds, NUM_LEDS, CHSV(0, 0, 0)); // Set all to Off.
-    FastLED.show();
-    delay(GAP_5);
+    start_hue_5 += delta_hue_5;
   }
 }
