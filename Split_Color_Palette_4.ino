96a78d7eefeddd682e90e91c227bc45c0e81909d
Kurt LaVacque <lavacquek@icloud.com>
2020-07-28 16:08:38 -0500

    Cleaned up some straglers

--- Split_Color_Palette_4.ino
+++ Split_Color_Palette_4.ino
@@ -5,22 +5,22 @@ void split_color_palette_4()
   int rate = 10;
   int split_value = 4;
   int row = NUM_LEDS / split_value;
-  int start_index_34;
-  int index_interval_34 = 5;
+  int start_index;
+  int index_interval = 5;
 
   CRGBPalette16 currentPalette = jet_gp;
-  start_index_34 = -1 * millis() / rate;
+  start_index = -1 * millis() / rate;
   for (int i = 0; i < NUM_LEDS / 2; i++)
 
   {
-    leds[i] = ColorFromPalette(currentPalette, start_index_34, BRIGHTNESS, LINEARBLEND);
+    leds[i] = ColorFromPalette(currentPalette, start_index, BRIGHTNESS, LINEARBLEND);
     for (int h = 2; h <= split_value - 2; h += 2)
     {
-      leds[backwards(row, h, i)] = ColorFromPalette(currentPalette, start_index_34, BRIGHTNESS, LINEARBLEND);
-      leds[forwards(row, h, i)] = ColorFromPalette(currentPalette, start_index_34, BRIGHTNESS, LINEARBLEND);
+      leds[backwards(row, h, i)] = ColorFromPalette(currentPalette, start_index, BRIGHTNESS, LINEARBLEND);
+      leds[forwards(row, h, i)] = ColorFromPalette(currentPalette, start_index, BRIGHTNESS, LINEARBLEND);
     }
-    leds[NUM_LEDS - 1 - i] = ColorFromPalette(currentPalette, start_index_34, BRIGHTNESS, LINEARBLEND);
-    start_index_34 += index_interval_34;
+    leds[NUM_LEDS - 1 - i] = ColorFromPalette(currentPalette, start_index, BRIGHTNESS, LINEARBLEND);
+    start_index += index_interval;
   };
   FastLED.show();
 }
